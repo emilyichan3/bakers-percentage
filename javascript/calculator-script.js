c384782933d7073b547d81a67605c6ee5cc6b503
Yi-Jung Chen <emilyichan3@gmail.com>
2024-07-13 11:54:09 +0100

    Added calculator page

---
+++ javascript/calculator-script.js
@@ -0,0 +1,70 @@
+const CALCULATOR_UNIT = 'g';
+const CALCULATOR_PERCENT ='%';
+var ingredientCount = 1;
+
+function submitFlourWeight(event) {
+  event.preventDefault();
+
+  const flourValue = document.getElementById('flourWeight');
+
+  if (flourValue.value === '') {
+    return;
+  } else {
+    addIngredientToTable('Flour', flourValue.value);
+  }
+
+  document.getElementById('addIngredient').addEventListener('click', addIngredient);
+}
+
+function addIngredientToTable(item, value){
+  let tbody = document.getElementById('calculator-ingredients');
+  let nameTd = document.createElement('td');
+  nameTd.innerText = item;
+
+  let metricTd = document.createElement('td');
+  metricTd.innerText = value + CALCULATOR_UNIT;
+  
+  let percentageTd = document.createElement('td');
+  percentageTd.innerText = value + CALCULATOR_PERCENT;
+
+  const deleteButton = createDeleteButton();
+
+  let ingredientTr = document.createElement('tr');
+  ingredientTr.setAttribute('id', 'row'+ingredientCount);
+  ingredientTr.append(nameTd, metricTd, percentageTd, deleteButton);
+
+
+  tbody.appendChild(ingredientTr);
+}
+
+function createDeleteButton() {
+  const deleteButton = document.createElement('button');
+  deleteButton.type = 'button';
+  // deleteButton.classList.add('btn', 'btn-danger', 'ms-auto');
+  deleteButton.setAttribute('id', 'row'+ingredientCount);
+  deleteButton.innerText = 'Delete';
+  
+  deleteButton.addEventListener('click', () => {
+    removeIngredient(deleteButton);
+  });
+
+  return deleteButton;
+}
+
+function removeIngredient(button){
+  let number = button.id;
+  console.log(number);
+  let row = document.getElementById(number);
+  row.remove();
+  // console.log(button);
+}
+function addIngredient(){
+  let ingredientName = document.getElementById('ingredientName');
+  let ingredientWeight = document.getElementById('ingredientWeight');
+  
+  addIngredientToTable(ingredientName.value, ingredientWeight.value);
+}
+
+addEventListener('load', () => {
+  document.querySelector('form').addEventListener('submit', submitFlourWeight);
+});
