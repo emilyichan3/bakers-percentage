41bb914a0bdde478831380b128524952abeb97ce
Yi-Jung Chen <emilyichan3@gmail.com>
2024-07-17 10:52:10 +0100

    Changed the name of Metric to Weight

--- javascript/data.js
+++ javascript/data.js
@@ -3,28 +3,28 @@ const cakeRecipes = [
     {
         name: 'Souffle Cheesecake',
         ingredients: [
-            { 'name': 'Cake flour', 'metric': 16, 'unit': 'g', 'percentage': 62 },
-            { 'name': 'Corn flour', 'metric': 10, 'unit': 'g', 'percentage': 38 },
-            { 'name': 'Cream cheese', 'metric': 120, 'unit': 'g', 'percentage': 462 },
-            { 'name': 'Butter', 'metric': 25, 'unit': 'g', 'percentage': 96 },
-            { 'name': 'Milk', 'metric': 100, 'unit': 'g', 'percentage': 385 },
-            { 'name': 'Egg yolk', 'metric': 44.4, 'unit': 'g', 'percentage': 171 },
-            { 'name': 'Egg white', 'metric': 105.9, 'unit': 'g', 'percentage': 407 },
-            { 'name': 'Sugar', 'metric': 50, 'unit': 'g', 'percentage': 192 }
+            { 'name': 'Cake flour', 'weight': 16, 'unit': 'g', 'percentage': 62 },
+            { 'name': 'Corn flour', 'weight': 10, 'unit': 'g', 'percentage': 38 },
+            { 'name': 'Cream cheese', 'weight': 120, 'unit': 'g', 'percentage': 462 },
+            { 'name': 'Butter', 'weight': 25, 'unit': 'g', 'percentage': 96 },
+            { 'name': 'Milk', 'weight': 100, 'unit': 'g', 'percentage': 385 },
+            { 'name': 'Egg yolk', 'weight': 44.4, 'unit': 'g', 'percentage': 171 },
+            { 'name': 'Egg white', 'weight': 105.9, 'unit': 'g', 'percentage': 407 },
+            { 'name': 'Sugar', 'weight': 50, 'unit': 'g', 'percentage': 192 }
         ]
     },
     {
         name: 'Castle Chocolate Cake',
         ingredients: [
-            { 'name': 'Cake flour', 'metric': 50, 'unit': 'g', 'percentage': 83 },
-            { 'name': 'Corn flour', 'metric': 10, 'unit': 'g', 'percentage': 17 },
-            { 'name': 'Cocoa powder', 'metric': 10, 'unit': 'g', 'percentage': 17 },
-            { 'name': 'Sugar', 'metric': 55, 'unit': 'g', 'percentage': 92 },
-            { 'name': 'Milk', 'metric': 50, 'unit': 'g', 'percentage': 83 },
-            { 'name': 'Oil', 'metric': 45, 'unit': 'g', 'percentage': 75 },
-            { 'name': 'Egg yolk', 'metric': 59.2, 'unit': 'g', 'percentage': 99 },
-            { 'name': 'Egg white', 'metric': 141.2, 'unit': 'g', 'percentage': 407 },
-            { 'name': 'Salt', 'metric': 2, 'unit': 'g', 'percentage': 3 }
+            { 'name': 'Cake flour', 'weight': 50, 'unit': 'g', 'percentage': 83 },
+            { 'name': 'Corn flour', 'weight': 10, 'unit': 'g', 'percentage': 17 },
+            { 'name': 'Cocoa powder', 'weight': 10, 'unit': 'g', 'percentage': 17 },
+            { 'name': 'Sugar', 'weight': 55, 'unit': 'g', 'percentage': 92 },
+            { 'name': 'Milk', 'weight': 50, 'unit': 'g', 'percentage': 83 },
+            { 'name': 'Oil', 'weight': 45, 'unit': 'g', 'percentage': 75 },
+            { 'name': 'Egg yolk', 'weight': 59.2, 'unit': 'g', 'percentage': 99 },
+            { 'name': 'Egg white', 'weight': 141.2, 'unit': 'g', 'percentage': 407 },
+            { 'name': 'Salt', 'weight': 2, 'unit': 'g', 'percentage': 3 }
         ]
     }
 ]
